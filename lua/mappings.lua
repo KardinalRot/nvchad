45a1bf7ddfcac2a78bb64ae253a297c63284c087
Michael Erlenbusch <michael.erlenbusch@veolia.com>
2025-12-11 14:34:38 +0100

    mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,12 +15,12 @@ map("n", "U", "<C-r>", { desc = "U for redo, the opposite of u for undo" })
 map("n", "c", '"_c', { desc = "change without yank" })
 map("n", "C", '"_C', { desc = "change without yank" })
 map("n", "<leader>d", '"_d', { desc = "delete without yank" })
+map("v", "<leader>d", '"_d', { silent = true })
 map("n", "<leader>k", ":d _<CR>", { desc = "Delete line without storing in clipboard" })
 map("n", "<leader>fm", ":TerraformFmt <CR>", { desc = "Terraform Format" })
 map("n", "<leader>ft", ":Telescope file_browser <CR>", { desc = "Telescope file_browser" })
 map("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Blame" })
 map("n", "<leader>jfm", ":%!jq .<CR>", { desc = "Format JSON with jq" })
-map("n", "<leader>u", ":UndotreeToggle<CR>", { desc = "Undotree Toggle" })
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 map("n", "J", "mzJ`z", { desc = "Join line without moving the cursor" })
@@ -39,6 +39,13 @@ map("i", "<C-j>", "<Down>", { desc = "Move down" })
 map("i", "<C-k>", "<Up>", { desc = "Move up" })
 -- Movements in insert mode !>
 
+map("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+map("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving up half-page"})
+
+map("n", "H", "^", {desc = "First non empty character"})
+map("n", "L", "$", {desc = "End of Line"})
+map("n", "M", "%")
+
   --   ["<leader>tt"] = {
   --     function()
   --       require("base46").toggle_theme()
@@ -46,4 +53,3 @@ map("i", "<C-k>", "<Up>", { desc = "Move up" })
   --     "Toggle Theme",
   --   },
 
-map("v", "<leader>d", '"_d', { silent = true })
